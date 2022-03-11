d9426439fb047f8fd8773866f18de59083c59108
bruno0011 <brunosouzviana@hotmail.com>
2022-03-11 15:49:58 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("disobedient");
+Console.WriteLine("");
+Console.ResetColor();
+
+Console.WriteLine("Good afternoon, sir");
+Console.WriteLine("What can I do, sir?");
+Console.WriteLine("Just say the word, sir");
+Console.WriteLine("Anything for you, sir");
+Console.WriteLine("Your friends all say, sir");
+Console.WriteLine("You don't deserve her");
+Console.WriteLine("I disagree, sir");
+Console.WriteLine("I live to serve, sir");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I think about all the wasted time I've spent");
+Console.WriteLine("I wanna be disobedient");
+Console.WriteLine("I stood awake wondering where my summers went");
+Console.WriteLine("I wanna be disobedient");
+Console.WriteLine("Disobedient, disobedient");
+Console.WriteLine("");
+Console.ResetColor();
+
+
+Console.WriteLine("I've been good, sir");
+Console.WriteLine("So very, very good for what?");
+Console.WriteLine("And I've given you");
+Console.WriteLine("Every single thing I've got");
+Console.WriteLine("It's feeling strange, man");
+Console.WriteLine("This whole arrangement");
+Console.WriteLine("Is gonna end with me totally deranged");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("When I think about all the wasted time I've spent");
+Console.WriteLine("I wanna be disobedient");
+Console.WriteLine("I stood awake wondering where my summers went");
+Console.WriteLine("I wanna be disobedient");
+Console.WriteLine("Disobedient, disobedient");
+Console.WriteLine("");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("I want to be disobedient");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("I want to be disobedient");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Disobedient, disobedient, disobedient");
+Console.ResetColor();
